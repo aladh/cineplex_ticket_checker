c08b155a5009920f03d7d59757375688c6633afa
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-06-30 01:58:53 -0400

    Get HTML for one movie

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+const baseURL = "https://www.cineplex.com/Movie/"
+
+func main() {
+	url := baseURL + flag.Arg(0)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Printf("error making request: %s\n", err)
+	}
+	defer res.Body.Close()
+
+	html, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("error reading response body: %s\n", err)
+	}
+
+	log.Printf("%s\n", html)
 }
